5f5ddc13c5787aec28875fde8f773ef0e3681052
khairislama <khairi.slama@polytechnicien.tn>
2021-02-14 18:32:35 +0100

    added Database

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-const   express = require("express"),
-        app = express(),
-        bodyParser = require("body-parser");
+const   express     = require("express"),
+        app         = express(),
+        bodyParser  = require("body-parser"),
+        mongoose    = require("mongoose");
 
-let campgrounds = [
-    {
-        name: "Salmon Creek",
-        image: "https://pixabay.com/get/g8c5b6cc7b6705d35cb5792fcec7b189b83c78063d97ba55057ada31250516bdb53d77565c3deeae882a27088fdc966db_340.jpg"
-    },
-    {
-        name: "Granit Hill",
-        image: "https://pixabay.com/get/geae749f76058df376e6bf12990223a87b350d8929f31c807e8c5a23c88e285f43e266147382bf33a6f97937e0e91e08e_340.jpg"
-    },
-    {
-        name: "Montain Goat's Rest",
-        image: "https://pixabay.com/get/g95703906910185d89bd053ac9a463caf45c17c2389c268e7fd58af1c64e3f985acec88ab19bbd8765d2c8f90c9034930_340.jpg"
-    }
-];
+mongoose.connect("mongodb://localhost/yelp_camp");
 
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+const Campground = mongoose.model("campground", campgroundSchema);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
@@ -26,7 +19,13 @@ app.get("/", (req, res)=>{
 });
 
 app.get("/campgrounds", (req, res)=>{
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, (err, allCampgrounds)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 app.get("/campgrounds/new", (req, res)=>{
@@ -37,10 +36,15 @@ app.post("/campgrounds", (req, res)=>{
     let name = req.body.name;
     let image = req.body.image;
     var newCampground = {name : name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    Campground.create(newCampground, (err, newCampground)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.listen(3000, () =>{
-    console.log("Serving is starting on port : 3000! ")
+    console.log("Serving is starting on port : 3000! ");
 });
